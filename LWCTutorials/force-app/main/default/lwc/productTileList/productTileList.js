48e95173e17f03b183b369445f13d5915144d2e5
heesooDK <heesoo.moon@dkbmc.com>
2022-03-14 18:01:25 +0900

    second section 14
    error fix need

---
+++ LWCTutorials/force-app/main/default/lwc/productTileList/productTileList.js
@@ -0,0 +1,37 @@
+import { LightningElement, wire, track } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
+import getProducts from '@salesforce/apex/ProductController.getProducts';
+import { registerListener, unregisterAllListeners, fireEvent } from 'c/pubsub';
+
+export default class ProductTileList extends LightningElement {
+    @track pageNumber = 1; pageSize; totlaItemCount = 0;
+    @track filters = {};
+    @wire(CurrentPageReference) pageRef;
+    @wire(getProducts, {filters: '$filters', pageNumber: '$pageNumber'}) products;
+    // @api;
+
+    connectedCallback() {
+        registerListener('filterChange', this.handleFilterChange, this);
+    }
+
+    handleProductSelected(event) {
+        fireEvent(this.pageRef, 'productSelected', event.detail);
+    }
+
+    disconnectedCallback() {
+        unregisterAllListeners(this);
+    }
+
+    handleFilterChange(filters) {
+        this.filters = {...filters};
+        this.pageNumber = 1;
+    }
+
+    handlePreviousPage() {
+        this.pageNumber -= 1;
+    }
+
+    handleNextPage() {
+        this.pageNumber += 1;
+    }
+}
